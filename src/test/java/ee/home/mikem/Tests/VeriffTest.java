037f56564820db39d121e1afbed4bb70ef519500
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-08-25 18:45:18 +0300

    Update Happy flow test Add Unhappy flow test

--- src/test/java/ee/home/mikem/Tests/VeriffTest.java
+++ src/test/java/ee/home/mikem/Tests/VeriffTest.java
@@ -1,6 +1,5 @@
 package ee.home.mikem.Tests;
 
-import ee.home.mikem.Libraries.Support;
 import ee.home.mikem.MainTest;
 import ee.home.mikem.Objects.Country;
 import ee.home.mikem.Objects.DocumentType;
@@ -8,6 +7,7 @@ import org.junit.Test;
 
 public class VeriffTest extends MainTest {
 
+    // Happy flow
     @Test
     public void should_show_nocamera_screen_when_verifying_by_passport_without_camera(){
 
@@ -15,7 +15,7 @@ public class VeriffTest extends MainTest {
         landingPage.checkPageIsOpened();
 
         // Enter full name
-        landingPage.enterFullName("Full name");
+        landingPage.enterFullName("Happy flow user");
 
         // Select country as Russia
         landingPage.selectDocumentCountry(Country.Russia);
@@ -30,14 +30,36 @@ public class VeriffTest extends MainTest {
         getVerifiedPage.checkPageIsOpened();
 
         // Passport as an option should be available
-        getVerifiedPage.verificationOptionShouldBeEnabled(DocumentType.passport);
+        getVerifiedPage.verificationOptionShouldBeEnabled(DocumentType.passport, true);
 
         // Tap Start button
         getVerifiedPage.tapStartButton();
 
         // No camera screen should be displayed
         noCameraPage.checkPageIsOpened();
+    }
 
-        Support.pause(1);
+    // Unhappy flow - we have error here: Drivers license tile is not displayed
+    @Test
+    public void should_disable_verification_when_verifying_by_id_without_id_reader(){
+
+        // Check landing page is opened
+        landingPage.checkPageIsOpened();
+
+        // Enter full name
+        landingPage.enterFullName("Unhappy flow user");
+
+        // Select document type as Passport
+        landingPage.selectDocumentType(DocumentType.drivingLicense);
+
+        // Tap Veriff me button
+        landingPage.tapSubmit();
+
+        // Get Verified page should be opened
+        getVerifiedPage.checkPageIsOpened();
+
+        getVerifiedPage.selectDocumentCountry(Country.Algeria);
+
+        getVerifiedPage.verificationOptionShouldBeEnabled(DocumentType.drivingLicense, false);
     }
 }
