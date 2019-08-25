15ad7a9a97146bafbf879051005f01d184f922b6
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-08-25 18:45:00 +0300

    Adde screen elements locators

--- src/test/java/ee/home/mikem/Pages/GetVerifiedPage.java
+++ src/test/java/ee/home/mikem/Pages/GetVerifiedPage.java
@@ -1,12 +1,16 @@
 package ee.home.mikem.Pages;
 
 import ee.home.mikem.Libraries.Support;
+import ee.home.mikem.Objects.DocumentType;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class GetVerifiedPage {
 
@@ -26,19 +30,22 @@ public class GetVerifiedPage {
     @FindBy(xpath = "//button[@class='button au-target']")
     private WebElement startButton;
 
+    // Country selector
+    @FindBy(xpath = "//select[@class='country-list au-target']")
+    private WebElement drpCountrySelector;
+
     public void checkPageIsOpened(){
 
         Assert.assertTrue("Landing page not opened", Support.waitForVisibility(pageHeader));
     }
 
-    public void verificationOptionShouldBeEnabled(String option){
+    public void verificationOptionShouldBeEnabled(String option, boolean isVisible){
 
-        Assert.assertTrue("Option '"+option+"' is not enabled",
-                Support.waitForVisibility(
-                        driver.findElement(By.xpath("//div[@class='label']")).findElement(By.xpath("//*[contains(text(), '"+option+"')]"))
-                )
-        );
-        Support.flash(driver.findElement(By.xpath("//div[@class='label']")).findElement(By.xpath("//*[text()='"+option+"']")));
+        List<WebElement> elements = driver.findElements(By.xpath("//label[@data-test-document-type='"+ DocumentType.getTypeCode(option) +"']"));
+        Assert.assertEquals(option+" option visibility should be: "+isVisible, isVisible, !elements.isEmpty());
+        if (isVisible) {
+            Support.flash(elements.get(0));
+        }
     }
 
     public void tapStartButton(){
@@ -46,5 +53,13 @@ public class GetVerifiedPage {
         Assert.assertTrue("Start button is not displayed", startButton.isDisplayed());
         Support.flash(startButton);
         startButton.click();
+    }
+
+    public void selectDocumentCountry(String documentCountry){
+
+        Assert.assertTrue("Document country drop down is not visible", drpCountrySelector.isDisplayed());
+        Support.flash(drpCountrySelector);
+        Select drpDocumentCountry = new Select(this.drpCountrySelector);
+        drpDocumentCountry.selectByVisibleText(documentCountry);
     }
 }
